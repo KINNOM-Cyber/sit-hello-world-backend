4de19f11130c83f2b07c023415025a7ffd997f1b
KINNOM-Cyber <kinnom254097@gmail.com>
2025-02-01 22:42:30 +0700

    Update test-route.js

--- routers/test-route.js
+++ routers/test-route.js
@@ -2,7 +2,7 @@ const {Router} = require('express')
 const router = Router()
 
 router.get("/test", (req, res) => {
-    res.send("Hello, World")
+    res.send("Hello form backend api!!!")
 })
 
 module.exports.testRoute = router
