c5a4d46361a51835852cb5e44f41d0e045f95e20
KINNOM-Cyber <kinnom254097@gmail.com>
2025-02-04 15:30:44 +0700

    remove test route

--- routers/index.js
+++ routers/index.js
@@ -2,13 +2,11 @@ const { Router } = require("express");
 const router = Router();
 
 // Import all route file here
-const { testRoute } = require("./test-route");
 const { bookingRoute } = require("./booking");
 
 const middleware = require("./middleware")
 
 // assign a route to express router
-router.use(testRoute);
 router.use("/booking", bookingRoute);
 // router.use(<imported path route>)
 
